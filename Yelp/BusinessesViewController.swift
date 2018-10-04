9593c47337daed4c24d8264a9fa6a2fabca04c02
IsaSam <isaacsamuel27@gmail.com>
2018-10-03 23:03:29 -0400

    Printing all PlaceHolder Label and BusinessDataName

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -4,20 +4,30 @@
 //
 //  Created by Timothy Lee on 4/23/15.
 //  Copyright (c) 2015 Timothy Lee. All rights reserved.
-//
+//  Copyright (c) 2018 Isaac Samuel. All rights reserved.
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var businesses: [Business]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.rowHeight = 120
+        tableView.estimatedRowHeight = 200
+        
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
                 self.businesses = businesses
+                self.tableView.reloadData()
+            
                 if let businesses = businesses {
                     for business in businesses {
                         print(business.name!)
@@ -43,6 +53,22 @@ class BusinessesViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //@available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        if businesses != nil {
+            return businesses!.count
+        }else{
+            return 0
+        }
+    }
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
+        
+        cell.business = businesses[indexPath.row]
+        
+        return cell
     }
     
     /*
